6bb360e261cb33ad717dee195c96eac464cbe2e2
MichaelGSanchez <michaelgregorysanchez@gmail.com>
2019-02-07 14:32:01 -0700

    Iterative palindrome test.

--- src/edu/cnm/deepdive/Palindrome.java
+++ src/edu/cnm/deepdive/Palindrome.java
@@ -2,16 +2,21 @@ package edu.cnm.deepdive;
 
 public class Palindrome {
 
-  public static boolean test (String candidate){
-  String simpleCandidate = candidate.toLowerCase().replaceAll("[^a-z0-9]", "");
-    //TODO Assign simpleCandidate from candidate, stripping ou spaces & punctuation.
+  public static boolean test(String candidate) {
+    String simpleCandidate = candidate.toLowerCase().replaceAll("[^a-z0-9]", "");
+    // TODO Assign simpleCandidate from candidate, stripping out spaces & punctuation.
     return testSimple(simpleCandidate);
   }
 
   private static boolean testSimple(String candidate) {
-    return (candidate.length() <= 1) ||
-        (candidate.charAt(0) == candidate.charAt(candidate.length() - 1)
-        && testSimple(candidate.substring(1, candidate.length() - 1)));
+    boolean result = true;
+    for (int i = 0, j = candidate.length() - 1; i < j; i++, j--) {
+      if (candidate.charAt(i) != candidate.charAt(j)) {
+        result = false;
+        break;
+      }
+    }
+    return result;
   }
 
 }
